94433ab22bacd29d4933df5edd92172d82c0baf2
Utku Doğrusöz <utku.dogrusoz@gmail.com>
2021-06-06 18:47:09 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+
+app.set("view engine", "ejs"),
+
+    //Middlewares Engine
+    app.use(express.static("public"));
+
+//routers 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Hoşgeldiniz",
-        status: 200
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+
+});
+app.get("/about", (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
     });
 
 });
